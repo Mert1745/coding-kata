a1dc591cf2cdb5b8ffa6f4e0c86133063dfc68f9
mkose <Mert.Kose@partner.bmwgroup.com>
2025-02-19 23:29:55 +0300

    Initial commit

---
+++ kata-backend/src/test/java/com/haiilo/kata/CodingKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haiilo.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
