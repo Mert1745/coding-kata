036d658958bbd8740ca3ef594bd643e3b8aa2a84
mkose <Mert.Kose@partner.bmwgroup.com>
2025-02-22 14:05:05 +0300

    HAIILO-3: Adjust cart component UI design and add "Proceed Checkout" button

--- kata-backend/src/main/resources/data.sql
+++ kata-backend/src/main/resources/data.sql
@@ -4,8 +4,8 @@ INSERT INTO Product (id, name) VALUES (2, 'Banana');
 INSERT INTO Product (id, name) VALUES (3, 'Orange');
 
 -- Insert into Price table (assuming the "product_id" column references the Product table)
-INSERT INTO Price (id, amount, quantity, product_id) VALUES (1, 3.40, 1, 1);
-INSERT INTO Price (id, amount, quantity, product_id) VALUES (2, 12.00, 4, 1);
-INSERT INTO Price (id, amount, quantity, product_id) VALUES (3, 5.00, 1, 2);
-INSERT INTO Price (id, amount, quantity, product_id) VALUES (4, 12.73, 3, 2);
-INSERT INTO Price (id, amount, quantity, product_id) VALUES (5, 7.26, 1, 3);
+INSERT INTO Price (id, amount, quantity, product_id) VALUES (1, 35, 1, 1);
+INSERT INTO Price (id, amount, quantity, product_id) VALUES (2, 120, 4, 1);
+INSERT INTO Price (id, amount, quantity, product_id) VALUES (3, 50, 1, 2);
+INSERT INTO Price (id, amount, quantity, product_id) VALUES (4, 120, 3, 2);
+INSERT INTO Price (id, amount, quantity, product_id) VALUES (5, 70, 1, 3);
--- kata-frontend/src/app/components/cart/cart.component.ts
+++ kata-frontend/src/app/components/cart/cart.component.ts
@@ -1,11 +1,60 @@
-import {Component, Input} from '@angular/core';
+import {ChangeDetectorRef, Component, Input} from '@angular/core';
 import {Cart} from "../../shared/interface";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrl: './cart.component.scss'
 })
 export class CartComponent {
-    @Input() carts: Cart[] | undefined;
+    @Input() carts: Cart[];
+    productSum: { productId: number, amount: number }[] = [];
+
+    constructor(private cdr: ChangeDetectorRef) {
+    }
+
+    calculateProductPrice(cart: Cart): number {
+        //Higher quantity will have to be calculated first as they have special offer.
+        const prices = cart.product.prices.sort((one, two) => (one.quantity > two.quantity ? -1 : 1));
+        let quantity = cart.quantity;
+        let sum = 0;
+
+        prices.forEach(price => {
+            let numberPerQuantity = Math.floor(quantity / price.quantity);
+            sum += (numberPerQuantity * price.amount);
+            quantity -= price.quantity * numberPerQuantity;
+        })
+
+        this.calculateProductSum(cart, sum);
+        return sum;
+    }
+
+    private calculateProductSum(cart: Cart, sum: number) {
+        const product = this.productSum.find(s => s.productId === cart.product.id);
+
+        if (!product) {
+            this.productSum.push({productId: cart.product.id, amount: sum});
+        } else {
+            product.amount = sum;
+        }
+        // Defer change detection to prevent infinite loop
+        setTimeout(() => this.cdr.detectChanges(), 0);
+    }
+
+    increaseProductQuantity(cart: Cart) {
+        cart.quantity = cart.quantity + 1;
+    }
+
+    decreaseProductQuantity(cart: Cart) {
+        cart.quantity = cart.quantity - 1;
+    }
+
+
+    getTotalSum(): number {
+        return this.productSum.reduce((total, product) => total + product.amount, 0);
+    }
+
+    proceedCheckout() {
+        console.log("checkout proceed ", this.productSum.length);
+    }
 }
